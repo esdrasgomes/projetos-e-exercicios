96a36b65565d793a35fd656e087a361c5dfbedb6
Esdras Gomes <esdrasgoomes@gmail.com>
2021-03-10 21:50:43 -0300

    tipo double

---
+++ java/exemploDouble/src/exemploDouble/exemploDouble.java
@@ -0,0 +1,16 @@
+package exemploDouble;
+
+public class exemploDouble {
+	public static void main(String[] args) {
+		
+		double nota1 = 8;
+		double nota2 = 7;
+		double nota3 = 10;
+		double nota4 = 9;
+		
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("A média do aluno é: " + mediaFinal);
+		
+	}
+}
