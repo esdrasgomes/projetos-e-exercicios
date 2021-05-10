7501fa734796eafe3448cddd95fa068cd81cd781
Esdras Gomes <esdrasgoomes@gmail.com>
2021-05-09 22:27:52 -0300

    created class FabricaCalculosMatematicos

---
+++ java/Calculadora/src/br/com/esdrasgomes/calculadora/fabricas/FabricaCalculosMatematicos.java
@@ -0,0 +1,25 @@
+package br.com.esdrasgomes.calculadora.fabricas;
+
+import br.com.esdrasgomes.calculadora.classes.Adicao;
+import br.com.esdrasgomes.calculadora.classes.Divisao;
+import br.com.esdrasgomes.calculadora.classes.Multiplicacao;
+import br.com.esdrasgomes.calculadora.classes.Subtracao;
+import br.com.esdrasgomes.calculadora.interfaces.CalculadorMatematico;
+
+public class FabricaCalculosMatematicos {
+	
+	public CalculadorMatematico criarCalculador(int numero1, int numero2, String calculo) {
+		if (calculo.equals("+")) {
+			return new Adicao(numero1, numero2);
+		} else if (calculo.equals("-")) {
+			return new Subtracao(numero1, numero2);
+		} else if (calculo.equals("*")) {
+			return new Multiplicacao(numero1, numero2);
+		} else if (calculo.equals("/")) {
+			return new Divisao(numero1, numero2);
+		} else {
+			return null;
+		}
+	}
+
+}
