1f0a67a0fba232d0cc55ef9ce4e1601ceab79c82
Esdras Gomes <esdrasgoomes@gmail.com>
2022-05-21 20:26:36 -0300

    Update project

--- php/Manipulação de Array/index.php
+++ php/Manipulação de Array/index.php
@@ -1,9 +1,25 @@
 <?php
     // ----------------------- Array Range -------------------------
-    $array = range(1, 20);
+    // $array = range(1, 20);
 
-    foreach($array as $item) {
-        echo $item."<br/>";
+    // foreach($array as $item) {
+    //     echo $item."<br/>";
+    // }
+
+    // ---------------------- key_exists ---------------------------
+    $array = [
+        'nome' => 'Esdras',
+        'age' => 22,
+        'empresa' => 'MV',
+        'cor' => 'Preto',
+        'profissao' => 'Desenvolvedor'
+    ];
+
+    if(key_exists('idade', $array)) {
+        $idade = $array['idade'];
+        echo $idade." anos";
+    } else {
+        echo "Não existe idade!";
     }
 
 ?>
