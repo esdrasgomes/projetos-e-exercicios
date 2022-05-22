c0e13818e78a827321d15ee2aba2cfa0e3f15509
Esdras Gomes <esdrasgoomes@gmail.com>
2022-05-22 15:16:07 -0300

    Update project

--- php/Manipulação de Array/index.php
+++ php/Manipulação de Array/index.php
@@ -7,6 +7,22 @@
     // }
 
     // ---------------------- key_exists ---------------------------
+    // $array = [
+    //     'nome' => 'Esdras',
+    //     'age' => 22,
+    //     'empresa' => 'MV',
+    //     'cor' => 'Preto',
+    //     'profissao' => 'Desenvolvedor'
+    // ];
+
+    // if(key_exists('idade', $array)) {
+    //     $idade = $array['idade'];
+    //     echo $idade." anos";
+    // } else {
+    //     echo "Não existe idade!";
+    // }
+
+    // ----------------- array_keys e array_values -----------------------
     $array = [
         'nome' => 'Esdras',
         'age' => 22,
@@ -15,11 +31,15 @@
         'profissao' => 'Desenvolvedor'
     ];
 
-    if(key_exists('idade', $array)) {
-        $idade = $array['idade'];
-        echo $idade." anos";
-    } else {
-        echo "Não existe idade!";
-    }
+    // pegando os objetos do array
+    // $chaves = array_keys($array);
+    // print_r($chaves);
 
+    // pegando os valores dos objetos do array
+    $chaves = array_values($array);
+    print_r($chaves);
+
+
+
+    
 ?>
